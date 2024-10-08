40f154da2c169ecb566ead709f716e7fccb07731
ITDAVID06 <don.hdavid@gmail.com>
2024-10-08 20:06:29 +0800

    completed lab9

--- index.php
+++ index.php
@@ -14,7 +14,10 @@ try {
     // Define routes
     $router->get('/', '\App\Controllers\HomeController@index');
     $router->get('/suppliers', '\App\Controllers\SupplierController@list');
+    $router->get('/customers', '\App\Controllers\CustomerController@list');
+    $router->get('/customers/{id}', '\App\Controllers\CustomerController@single');
     $router->get('/suppliers/{id}', '\App\Controllers\SupplierController@single');
+    $router->post('/customers/{id}', '\App\Controllers\CustomerController@update');
     $router->post('/suppliers/{id}', '\App\Controllers\SupplierController@update');
 
     // Run it!
--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -11,7 +11,7 @@ class HomeController extends BaseController
     {
         $template = 'home';
         $data = [
-            'student' => 'REPLACE THIS WITH YOUR FULL NAME',
+            'student' => 'DON HENESSY S. DAVID',
             'title' => 'IPT10 Laboratory Activity #9',
             'college' => 'College of Computer Studies',
             'university' => 'Angeles University Foundation',
